3bc0578051439728432aeaaa2fe7dc3cccfc67a1
halle <severin.haller@stud.kswe.ch>
2021-09-15 15:02:01 +0200

    Committed

---
+++ Object Oriented Programming/Object Oriented Programming 2/1_funktion_zur_methode.py
@@ -0,0 +1,12 @@
+import roboter
+
+roboter_1 = roboter.Roboter()
+#roboter_1.name = "WALL-E"
+#roboter_1.baujahr = 2008
+roboter_2 = roboter.Roboter()
+roboter_2.name = "HAL 9000"
+roboter_2.baujahr = 1997
+roboter_2 = roboter.Roboter()
+roboter_2.sage_hallo()
+print(roboter_1.__dict__)
+print(roboter_1)
---
+++ Object Oriented Programming/Object Oriented Programming 2/roboter.py
@@ -0,0 +1,11 @@
+class Roboter:
+    def sage_hallo(self):
+        self.name = 1
+        print("Hallo, ich heisse", self.name)
+
+
+    def __init__(self):
+       # self.name = input("Name: ")
+       # self.baujahr = input("Baujahr: ")
+        pass
+
